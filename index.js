a1b3f4f4440c5a4d5825228b5c7f3d8accf5c12b
marufr1 <marufrahman923@gmail.com>
2022-01-03 12:39:39 +0700

    Modify app.use, app.set, app.get in index.js. And add link css in index.ejs

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const PORT = 3000
 //Menjadikan variabel app sebagai module express
 const app = express()
 
+//Memanggil fungsi appInit(app)
+appInit(app)
+
 app.listen(PORT, () => {
     console.log('Listening to port 3000...')
 })
+
+function appInit(app){
+    app.use(express.static('app/public'))
+    
+    app.set('views', 'app/views')
+    app.set('view engine', 'ejs')
+    
+    //Memanggil file .ejs
+    app.get('/', (req, res) => {
+        res.render('index')
+    })
+}
