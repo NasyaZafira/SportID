2b347fa2c246237e13adf586e6a47e4841d834ab
marufr1 <marufrahman923@gmail.com>
2022-01-04 23:07:46 +0700

    Modify css for dropdown menu. Add icon arrow_down to public. Maintain between Frontend side and Backend side

--- app/migrations/20211230095511-create-laporan.js
+++ app/migrations/20211230095511-create-laporan.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('laporans', {
+    await queryInterface.createTable('laporan', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 // load the things we need
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
 const { Sequelize } = require('sequelize')
 const router = require('./app/router/router')
 const port = 3000;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(router)
-
-//Static Files
-app.use( express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/icon', express.static(__dirname + 'public/icon'))
-app.use('/fonts', express.static(__dirname + 'public/fonts'))
-// use res.render to load up an ejs view file
-// bagian Register Page
-app.get('/', function(req, res) {
-    res.render('pages/register');
-});
+var app = express();
+appInit(app)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
+
+function appInit(app) {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+
+    //Static Files
+    app.use( express.static('app/public'))
+
+    // set the view engine to ejs
+    app.set('views', 'app/views')
+    app.set('view engine', 'ejs');
+
+    // use res.render to load up an ejs view file
+    // bagian Register Page
+    app.get('/', function(req, res) {
+        res.render('pages/register');
+    });
+}
