ef95f3fc7e39cfb38f3b749f363d3544717bd51f
gilang_wahyudi <gilangwahjoedhi@gmail.com>
2022-01-05 20:31:40 +0700

    ubah tata letak file

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const PORT = 3000
 //Menjadikan variabel app sebagai module express
 const app = express()
 
+//Static Files
+app.use(express.static('app/public'))
+
+// set the view engine to ejs
+app.set('views', 'app/views');
+app.set('view engine', 'ejs');
+
+//Halaman homepage
+app.get('/homepagelight', function(req, res){
+    res.render('pages/Homepage-light')
+});
+app.get('/homepagedark', function(req, res){
+    res.render('pages/Homepage-dark')
+});
+
 app.listen(PORT, () => {
     console.log('Listening to port 3000...')
 })
