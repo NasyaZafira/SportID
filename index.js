6965eda32f26109c96bb59ef35b17ef018900f0a
krissyvieri <krissy.vieri@gmail.com>
2022-01-06 22:29:20 +0700

    update kotak login, header dan footer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const { Sequelize } = require('sequelize')
+const {
+    Sequelize
+} = require('sequelize')
 const PORT = 3000
 
 //Menjadikan variabel app sebagai module express
@@ -7,4 +9,17 @@ const app = express()
 
 app.listen(PORT, () => {
     console.log('Listening to port 3000...')
+})
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.static('app/public'));
+app.set('views', 'app/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('login')
 })
