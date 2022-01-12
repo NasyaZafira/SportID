3e51627d2c18f3f1979ca5d5de64cc88b5858c9d
krissyvieri <krissy.vieri@gmail.com>
2022-01-12 20:33:23 +0700

    update  profilePage

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ appInit(app)
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`)
-    const db = new Sequelize(
-        config.development.database,
-        config.development.username,
-        config.development.password, {
-            host: config.development.host,
-            port: config.development.port,
-            dialect: config.development.dialect,
-            dialectOptions: config.development.dialectOptions,
-            logging: false
-        }
-    )
-    db.authenticate().then(console.log('Berhasil koneksi ke database'))
-        .catch(err => {
-            console.log('Gagal koneksi ke local database', err)
-            process.exit()
-        });
+    // const db = new Sequelize(
+    //     config.development.database,
+    //     config.development.username,
+    //     config.development.password, {
+    //         host: config.development.host,
+    //         port: config.development.port,
+    //         dialect: config.development.dialect,
+    //         dialectOptions: config.development.dialectOptions,
+    //         logging: false
+    //     }
+    // )
+    // db.authenticate().then(console.log('Berhasil koneksi ke database'))
+    //     .catch(err => {
+    //         console.log('Gagal koneksi ke local database', err)
+    //         process.exit()
+    //     });
 });
 
 
