15c0cc466aee27d4bf3c5d4d8f21bb79c397ed95
marufr1 <marufrahman923@gmail.com>
2022-01-14 11:23:25 +0700

    Modifikasi data.json dan membuat seeder

--- app/seeders/20220113183733-seed-berita.js
+++ app/seeders/20220113183733-seed-berita.js
@@ -3,17 +3,19 @@ const sourceBerita = require('../models/data.json')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // const mappedProducts = sourceBerita.arrayOfProducts.map((produk) => {
-    //   return {
-    //     imgUrl: produk.imgUrl,
-    //     name: produk.name,
-    //     price: produk.price,
-    //     categories: produk.category,
-    //     subcategory: produk.subcategory,
-    //     createdAt: new Date(),
-    //     updatedAt: new Date(),
-    //   };
-    // }); ==> (LINE 6 SAMPAI 16 DIKETIK SESUAI ISI DARI models/data.json)
+    const mappedBerita = sourceBerita.arrayOfProducts.map((berita) => {
+      return {
+        imageBerita: berita.imageBerita,
+        judulBerita: berita.judulBerita,
+        isiBerita: berita.isiBerita,
+        kategori: berita.kategori,
+        admin_name: berita.admin_name,
+        isTrending: berita.isTrending,
+        createdAt: berita.createdAt,
+        updatedAt: berita.updatedAt
+      };
+    }); //==> (LINE 6 SAMPAI 16 DIKETIK SESUAI ISI DARI models/data.json)
+    await queryInterface.bulkInsert('berita', mappedBerita, {});
     /**
      * Add seed commands here.
      *
@@ -26,7 +28,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    // await queryInterface.bulkDelete('Products', null, {}); ==> (Ditulis sesuai isi dari models/data.json)
+    // await queryInterface.bulkDelete('Products', null, {}); //==> (Ditulis sesuai isi dari models/data.json)
     /**
      * Add commands to revert seed here.
      *
